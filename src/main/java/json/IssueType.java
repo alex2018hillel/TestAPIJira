fc28dd285bc888a0c7768fc16ff001addbf084a6
alex2018hillel <alex2018hillel@gmail.com>
2019-07-03 13:35:01 +0300

    add JSON2

--- src/main/java/json/IssueType.java
+++
@@ -1,9 +0,0 @@
-package json;
-
-public class IssueType implements Pojo {
-    public String id;
-
-    public IssueType(String id) {
-        this.id = id;
-    }
-}
--- src/main/java/json/Project.java
+++
@@ -1,9 +0,0 @@
-package json;
-
-public class Project implements Pojo {
-    public String id;
-
-    public Project(String id) {
-        this.id = id;
-    }
-}
